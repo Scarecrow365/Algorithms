3bddaa28347e9a91332952e53dc1341039140c05
DmitryBychkov <bychkov_dmitry@bk.ru>
2024-10-14 23:25:03 +0300

    Added Levenshtein distance algorithm

---
+++ Assets/Scripts/DistanceLevenshtein/DistanceLevenshtein.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DistanceLevenshtein
+{
+    public class DistanceLevenshtein
+    {
+        public int Execute(string firstWord, string secondWord)
+        {
+            if (string.IsNullOrEmpty(firstWord) || string.IsNullOrEmpty(secondWord)) 
+                throw new Exception("The field is empty");
+            
+            return DirectMethod(firstWord, secondWord);
+        }
+
+        private static int DirectMethod(string firstWord, string secondWord)
+        {
+            int firstWordLength = firstWord.Length + 1;
+            int secondWordLength = secondWord.Length + 1;
+            int[,] matrixD = new int[firstWordLength, secondWordLength];
+
+            const int deletionCost = 1;
+            const int insertionCost = 1;
+
+            for (int i = 0; i < firstWordLength; i++)
+            {
+                matrixD[i, 0] = i;
+            }
+            
+            for (int i = 0; i < secondWordLength; i++)
+            {
+                matrixD[0, i] = i;
+            }
+
+            for (int i = 1; i < firstWordLength; i++)
+            {
+                for (int j = 1; j < secondWordLength; j++)
+                {
+                    int substitutionCost = firstWord[i - 1] == secondWord[j - 1] ? 0 : 1;
+
+                    matrixD[i, j] = Minimum(
+                        matrixD[i - 1, j] + deletionCost,
+                        matrixD[i, j - 1] + insertionCost,
+                        matrixD[i - 1, j - 1] + substitutionCost
+                    );
+                }
+            }
+
+            return matrixD[firstWordLength - 1, secondWordLength - 1];
+        }
+
+        private static int Minimum(int a, int b, int c) => (a = a < b ? a : b) < c ? a : c;
+    }
+}
---
+++ Assets/Scripts/Editor/DistanceLevenshteinEditor.cs
@@ -0,0 +1,72 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+  public class DistanceLevenshteinEditor : EditorWindow
+  {
+    public string firstWord;
+    public string secondWord;
+    
+    private SerializedObject _obj;
+    private SerializedProperty _propFirstWord;
+    private SerializedProperty _propSecondWord;
+        
+    [MenuItem("Tools/Distance Levenshtein")]
+    public static void ShowWindow()
+    {
+      var window = GetWindow(typeof(DistanceLevenshteinEditor), true, "Distance Levenshtein");
+      window.position.Set(window.position.x, window.position.y, 300, 0);
+      window.Show();
+    }
+        
+    private void OnEnable()
+    {
+      SetProps();
+    }
+
+    private void SetProps()
+    {
+      _obj = new SerializedObject(this);
+      _propFirstWord = _obj.FindProperty(nameof(firstWord));
+      _propSecondWord = _obj.FindProperty(nameof(secondWord));
+    }
+
+    private void OnGUI()
+    {
+      _obj.Update();
+      DrawBlockGUI("First word", _propFirstWord);
+      DrawBlockGUI("Second word", _propSecondWord);
+      DrawButtonGUI();
+
+      if (_obj.ApplyModifiedProperties())
+      {
+        SceneView.RepaintAll();
+      }
+    }
+    
+    private void DrawButtonGUI()
+    {
+      EditorGUILayout.Separator();
+      if (GUILayout.Button(nameof(Execute)))
+      {
+        Execute();
+      }
+    }
+
+    private static void DrawBlockGUI(string name, SerializedProperty prop)
+    {
+      EditorGUILayout.BeginHorizontal("box");
+      EditorGUILayout.LabelField(name, GUILayout.Width(100));
+      EditorGUILayout.PropertyField(prop, GUIContent.none);
+      EditorGUILayout.EndHorizontal();
+    }
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    private void Execute()
+    {
+      var algorithm = new DistanceLevenshtein.DistanceLevenshtein();
+      Debug.Log($"Result of Levenshtein: {algorithm.Execute(firstWord, secondWord).ToString()}");
+    }
+  }
+}
