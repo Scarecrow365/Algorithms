bf4fee9e690260dc79979846403376252270cdb0
DmitryBychkov <bychkov_dmitry@bk.ru>
2024-10-15 22:22:12 +0300

    Added shell sort and fixed stooge sort

---
+++ Assets/Scripts/Sorts/SortType/ShellSort.cs
@@ -0,0 +1,27 @@
+﻿namespace Sorts.SortType
+{
+  public class ShellSort : BaseSort
+  {
+    public override int[] Execute(int[] array)
+    {
+      int halfArrayLength = array.Length / 2;
+
+      while (halfArrayLength >= 1)
+      {
+        for (int i = halfArrayLength; i < array.Length; i++)
+        {
+          int j = i;
+          while (j >= halfArrayLength && array[j - halfArrayLength] > array[j])
+          {
+            Swap(ref array[j], ref array[j - halfArrayLength]);
+            j -= halfArrayLength;
+          }
+        }
+        
+        halfArrayLength /= 2;
+      }
+
+      return array;
+    }
+  }
+}
--- Assets/Scripts/Sorts/SortType/StoogeSort.cs
+++ Assets/Scripts/Sorts/SortType/StoogeSort.cs
@@ -4,22 +4,23 @@
   {
     public override int[] Execute(int[] array)
     {
-      RecursiveExecute(array, 0, array[^1]);
-      return array;
+      return RecursiveExecute(array, 0, array.Length - 1);
     }
 
-    private static void RecursiveExecute(int[] array, int startIndex, int endIndex)
+    private static int[] RecursiveExecute(int[] array, int startIndex, int endIndex)
     {
       if (array[startIndex] > array[endIndex])
         Swap(ref array[startIndex], ref array[endIndex]);
 
-      if (endIndex - startIndex <= 1)
-        return;
-
+      if (endIndex - startIndex <= 1) 
+        return array;
+      
       int len = (endIndex - startIndex + 1) / 3;
       RecursiveExecute(array, startIndex, endIndex - len);
       RecursiveExecute(array, startIndex + len, endIndex);
       RecursiveExecute(array, startIndex, endIndex - len);
+      
+      return array;
     }
   }
 }
