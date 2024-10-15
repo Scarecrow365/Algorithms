d89bbdae2c5c9e1ea150674996c0bb95f64fb818
DmitryBychkov <bychkov_dmitry@bk.ru>
2024-10-15 22:39:56 +0300

    Added quick sort

---
+++ Assets/Scripts/Sorts/SortType/QuickSort.cs
@@ -0,0 +1,39 @@
+﻿namespace Sorts.SortType
+{
+  public class QuickSort : BaseSort
+  {
+    public override int[] Execute(int[] array)
+    {
+      return Sort(array, 0, array.Length - 1);
+    }
+
+    private static int[] Sort(int[] array, int minIndex, int maxIndex)
+    {
+      if (minIndex >= maxIndex) return array;
+
+      int pivotIndex = Partition(array, minIndex, maxIndex);
+      Sort(array, minIndex, pivotIndex - 1);
+      Sort(array, pivotIndex + 1, maxIndex);
+
+      return array;
+    }
+
+    private static int Partition(int[] array, int minIndex, int maxIndex)
+    {
+      int pivot = minIndex - 1;
+
+      for (int i = minIndex; i < maxIndex; i++)
+      {
+        if (array[i] >= array[maxIndex])
+          continue;
+
+        pivot++;
+        Swap(ref array[pivot], ref array[i]);
+      }
+
+      pivot++;
+      Swap(ref array[pivot], ref array[maxIndex]);
+      return pivot;
+    }
+  }
+}
