b471582ee856bc2cd1ede5fc33639a3f4faf06ba
DmitryBychkov <bychkov_dmitry@bk.ru>
2024-10-15 21:59:38 +0300

    Added pancake sort

---
+++ Assets/Scripts/Sorts/SortType/PancakeSort.cs
@@ -0,0 +1,38 @@
+﻿namespace Sorts.SortType
+{
+  public class PancakeSort : BaseSort
+  {
+    public override int[] Execute(int[] array)
+    {
+      for (int subArrayLength = array.Length - 1; subArrayLength >= 0; subArrayLength--)
+      {
+        int indexOfMax = IndexOfMax(array, subArrayLength);
+
+        if (indexOfMax == subArrayLength)
+          continue;
+
+        Flip(array, indexOfMax);
+        Flip(array, subArrayLength);
+      }
+
+      return array;
+    }
+
+    private static int IndexOfMax(int[] array, int index)
+    {
+      var result = 0;
+
+      for (var i = 1; i <= index; ++i)
+        if (array[i] > array[result])
+          result = i;
+
+      return result;
+    }
+
+    private static void Flip(int[] array, int end)
+    {
+      for (var start = 0; start < end; start++, end--) 
+        Swap(ref array[start], ref array[end]);
+    }
+  }
+}
