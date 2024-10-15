b3f421a5f4408dd92fb9387a9eef3f8598f16672
DmitryBychkov <bychkov_dmitry@bk.ru>
2024-10-15 21:33:24 +0300

    Added insertion sort

--- Assets/Scripts/Sorts/SortType/BaseSort.cs
+++ Assets/Scripts/Sorts/SortType/BaseSort.cs
@@ -1,9 +1,9 @@
-﻿namespace Sorts
+﻿namespace Sorts.SortType
 {
   public abstract class BaseSort
   {
     public abstract int[] Execute(int[] array);
-    
+
     protected static void Swap(ref int i, ref int j) => (i, j) = (j, i);
   }
 }
---
+++ Assets/Scripts/Sorts/SortType/InsertionSort.cs
@@ -0,0 +1,21 @@
+﻿namespace Sorts.SortType
+{
+  public class InsertionSort : BaseSort
+  {
+    public override int[] Execute(int[] array)
+    {
+      for (var i = 1; i < array.Length; i++)
+      {
+        int key = array[i];
+        int j = i;
+        while (j > 1 && array[j - 1] > key)
+        {
+          Swap(ref array[j - 1], ref array[j]);
+          j--;
+        }
+      }
+
+      return array;
+    }
+  }
+}
