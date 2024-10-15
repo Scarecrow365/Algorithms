218b52020d46b333c92c1412bca2219340fbe37a
DmitryBychkov <bychkov_dmitry@bk.ru>
2024-10-15 21:51:45 +0300

    Added stooge sort

---
+++ Assets/Scripts/Sorts/SortType/StoogeSort.cs
@@ -0,0 +1,25 @@
+﻿namespace Sorts.SortType
+{
+  public class StoogeSort : BaseSort
+  {
+    public override int[] Execute(int[] array)
+    {
+      RecursiveExecute(array, 0, array[^1]);
+      return array;
+    }
+
+    private static void RecursiveExecute(int[] array, int startIndex, int endIndex)
+    {
+      if (array[startIndex] > array[endIndex])
+        Swap(ref array[startIndex], ref array[endIndex]);
+
+      if (endIndex - startIndex <= 1)
+        return;
+
+      int len = (endIndex - startIndex + 1) / 3;
+      RecursiveExecute(array, startIndex, endIndex - len);
+      RecursiveExecute(array, startIndex + len, endIndex);
+      RecursiveExecute(array, startIndex, endIndex - len);
+    }
+  }
+}
